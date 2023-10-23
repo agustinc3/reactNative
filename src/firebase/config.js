0d9d907b26040ec7730c6cece4e79141b9f8045a
agustinc3 <hcagnoli@udesa.edu.ar>
2023-10-23 09:56:19 -0300

    agrego proyecto

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDipALj1owxxhe2FXI7HqqCKAYtqy-sUDE",
+  authDomain: "proyectointegrador2-1c1ab.firebaseapp.com",
+  projectId: "proyectointegrador2-1c1ab",
+  storageBucket: "proyectointegrador2-1c1ab.appspot.com",
+  messagingSenderId: "356149005685",
+  appId: "1:356149005685:web:8e8c5caf166c41339b973c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
